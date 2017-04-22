538e98905662f3c6a0e8c29fc119f6e8b7c3bc81
yuhei <yuhei.ariyama@gmail.com>
2017-04-22 23:52:29 +0900

    追加

---
+++ UnitTestProject/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using WpfTest;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Employee e = new Employee();
+            Assert.AreEqual(e.Add(1, 2), 3);
+        }
+    }
+}
---
+++ WpfTests/EmployeeTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using WpfTest;
+
+namespace WpfTests
+{
+    [TestClass]
+    public class EmployeeTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+        }
+    }
+}
