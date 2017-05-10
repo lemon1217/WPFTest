0bd03932c3721074a7fb69c60f2177fcbbabba81
lemon1217 <Yuhei.Ariyama@gmail.com>
2017-05-11 01:02:25 +0900

    [add]WpfTestDll

---
+++ WpfTestDll/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfTestDll
+{
+    public class Employee
+    {
+        // 実装は外部から隠蔽(privateにしておく)
+        private int id;
+        public int Id
+        {
+            protected set { this.id = value; }
+            get { return this.id; }
+        }
+
+        private string name;
+        public string Name
+        {
+            protected set { this.name = value; }
+            get { return this.name; }
+        }
+
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
